f97ae6332682694bbbc1250381fd4b187d99a50a
Josh Bowers <josh.bowers.usa@gmail.com>
2019-05-23 20:18:46 -0600

    added sopurces for reference

--- node/functionalProgramming/fr.js
+++ node/functionalProgramming/fr.js
@@ -1,3 +1,15 @@
+/**
+ * Sources
+ * General Ideas
+ * https://medium.com/javascript-scene/curry-and-function-composition-2c208d774983
+ *
+ * currying
+ * https://stackoverflow.com/questions/27996544/how-to-correctly-curry-a-function-in-javascript#answer-30249365
+ *
+ * partial currying (useful if you need to get it down to the second to last function)
+ * https://stackoverflow.com/questions/42164000/how-to-reconcile-javascript-with-currying-and-function-composition/42164779#42164779
+ *
+ */
 "use strict";
 
 /**
