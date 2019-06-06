9c5bfbda0ebeea2cf5ed2e3f39cf1c2877f90952
Josh Bowers <josh.bowers.usa@gmail.com>
2019-06-06 01:05:38 -0600

    added helpful set functions

--- node/sets/setHelpers.js
+++ node/sets/setHelpers.js
@@ -32,3 +32,61 @@ function uniqueSet( SOURCE ){
 // let end = new Date().getTime();
 //
 // console.log( end - start );
+
+//useful set functions
+
+
+function toArray( set ){
+    return[ ...set ];
+}
+
+
+// functions taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+function isSuperset(set, subset) {
+    for (let elem of subset) {
+        if (!set.has(elem)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function union(setA, setB) {
+    const _union = new Set(setA);
+    for (let elem of setB) {
+        _union.add(elem);
+    }
+    return _union;
+}
+
+function intersection(setA, setB) {
+    const _intersection = new Set();
+    for (let elem of setB) {
+        if (setA.has(elem)) {
+            _intersection.add(elem);
+        }
+    }
+    return _intersection;
+}
+
+function symmetricDifference(setA, setB) {
+    const _difference = new Set(setA);
+    for (let elem of setB) {
+        if (_difference.has(elem)) {
+            _difference.delete(elem);
+        } else {
+            _difference.add(elem);
+        }
+    }
+    return _difference;
+}
+
+function difference(setA, setB) {
+    const _difference = new Set(setA);
+    for (let elem of setB) {
+        _difference.delete(elem);
+    }
+    return _difference;
+}
+
