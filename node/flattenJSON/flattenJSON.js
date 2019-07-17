9457045d01ae840b531e1681649f75eaf02a6683
Josh Bowers <josh.bowers.usa@gmail.com>
2019-07-17 09:28:06 -0600

    added benchmarking function

--- node/flattenJSON/flattenJSON.js
+++ node/flattenJSON/flattenJSON.js
@@ -22,15 +22,17 @@ JSON.unflatten = function(data) {
     return result[""];
 };
 
-JSON.flatten = function(data) {
+// convert to a none recursive function and see if there are any performance gains
+JSON.flattenRecursive = function(data) {
     let result = {};
     function recurse (cur, prop) {
         if (Object(cur) !== cur) {
             result[prop] = cur;
         } else if (Array.isArray(cur)) {
-            for(var i=0, l=cur.length; i<l; i++)
+            let l = cur.length;
+            for(let i = 0; i < l; ++i)
                 recurse(cur[i], prop ? prop+"."+i : ""+i);
-            if (l == 0)
+            if (l === 0)
                 result[prop] = [];
         } else {
             let isEmpty = true;
@@ -46,8 +48,93 @@ JSON.flatten = function(data) {
     return result;
 };
 
-let flat = JSON.flatten( { a:{ b:{ c:[ { aa:{ aaa:1, aab:"", aac:{}, aad:[] }, ab: [ 1, 2, 3, 4 ], ac:"hi there", ad:1 }, { a: "lol", b: 123 }, new Date(), "hi", "there", "again", 1, 2,3 ] } } } );
+// let flat = JSON.flattenRecursive( { a:{ b:{ c:[ { aa:{ aaa:1, aab:"", aac:{}, aad:[] }, ab: [ 1, 2, 3, 4 ], ac:"hi there", ad:1 }, { a: "lol", b: 123 }, new Date(), "hi", "there", "again", 1, 2,3 ] } } } );
+//
+// console.log( flat );
+// console.log( "========================================" );
+// console.log( JSON.stringify( JSON.unflatten( flat ) ) );
 
-console.log( flat );
-console.log( "========================================" );
-console.log( JSON.stringify( JSON.unflatten( flat ) ) );
+function benchmark( flattenFunc, unflattenFunc, testDataDepth=4 ) {
+
+    let stats = require( "./../stats/base.js" );
+
+    let tests = 10;
+    let trials = 10000;
+
+    let nestedObjArr = [];
+    let flatObjArr = [];
+
+    console.log( "Creating test data" );
+    for( let i = 0; i < trials; ++i ){
+        nestedObjArr.push( fillObj( {}, testDataDepth ) );
+
+        let temp = fillObj( {}, testDataDepth );
+        flatObjArr.push( JSON.flattenRecursive( temp ) )
+    }
+
+    console.log( "Starting flattenRecursive tests" );
+    let flattenTests = runTest( flattenFunc, nestedObjArr );
+
+    console.log( "Starting unflatten tests" );
+    let nestedTests = runTest( unflattenFunc, flatObjArr );
+
+    return {
+        fromNested: getTestStats( flattenTests ),
+        fromFLat: getTestStats( nestedTests )
+    };
+
+    function runTest( func, dataArr ){
+        let start = 123456789;
+        let end = 123456789;
+        let testTimes = [];
+
+        for( let i = 0; i < tests + 1; ++i ){
+
+            start = new Date().getTime();
+
+            for( let j = 0; j < trials; ++j ){
+                func( dataArr[j] );
+            }
+
+            end = new Date().getTime();
+
+            if( i - 1 > -1 )
+                testTimes.push( ( trials / ( end - start ) ) * 1000 );
+        }
+
+        return testTimes;
+    }
+
+
+    function getTestStats( arrOfTimes ){
+        return {
+            // tests: arrOfTimes,
+            avg: Math.floor( stats.getAverage( arrOfTimes ).toLocaleString( "en-US" ) + " ops per second." ),
+            median: Math.floor( stats.getMedian( arrOfTimes ).toLocaleString( "en-US" ) + " ops per second." ),
+            std: Math.floor(stats.getStandardDeviation( arrOfTimes ).toLocaleString( "en-US" ) + " ops per second." )
+        }
+    }
+}
+
+function fillObj(obj, depth) {
+    obj["foo0"] = 1;
+    obj["bar0"] = false;
+    obj["foo2"] = -99999;
+    obj["bar2"] = true;
+    obj["foo3"] = 10002525.10002525;
+    obj["bar3"] = "test";
+    obj["foo4"] = "hello world hello world hello world hello world";
+    obj["bar4"] = [1,2,3,4,5,6,7,8,9,0];
+    if(depth > 0) {
+        obj["foo5"] = {};
+        fillObj(obj["foo5"], depth-1);
+        obj["bar5"] = [{},{},{},{},{}];
+        for(var i=0; i<obj["bar5"].length; i++) {
+            fillObj(obj["bar5"][i], depth-1);
+        }
+    }
+    return obj;
+}
+
+// console.log( JSON.flattenRecursive( fillObj( {}, 2 ) ) );
+console.log( benchmark( JSON.flattenRecursive, JSON.unflatten, 2 ) );
