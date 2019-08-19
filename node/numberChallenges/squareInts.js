06fbee8b118d1e4964c94e00be6c79f715bf1ccb
josh bowers <josh.bowers.usa@gmail.com>
2019-08-19 16:29:45 -0600

    tweaked logs

--- node/numberChallenges/squareInts.js
+++ node/numberChallenges/squareInts.js
@@ -3,25 +3,29 @@
  * For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
  *  https://www.codewars.com/kata/square-every-digit?source=post_page-----72655b2e5edd----------------------
  */
+function getNumOfDigits( int ){
+    return Math.floor( Math.log10( Math.abs( int ) ) );
+}
 
 
 function squareDigits( int ){
     let res = [];
-    let digitCount = Math.floor( Math.log10( Math.abs( int ) ) ) + 1;
+    let digitCount = getNumOfDigits( int );
     let digit = 0;
-    let pow = 0;
+    let squaredDigit = 0;
 
     for( let i = digitCount; i >= 0; --i ){
-        pow = Math.pow( 10, i );
-        digit = Math.floor( ( Math.abs( int ) / pow ) % 10 );
-        res.push( Math.pow( digit, 2 ) );
+        digit = Math.floor( ( Math.abs( int ) / Math.pow( 10, i ) ) % 10 );
+        squaredDigit = Math.pow( digit, 2 );
+        res.push( squaredDigit );
     }
 
     return Number.parseInt( res.join("") );
 }
 
 console.log( "Square Digits" );
-console.log( squareDigits( 9119  ) ); // => 811181
+console.log( "9119 =>", squareDigits( 9119 ) ); // => 811181
+console.log( "123 =>", squareDigits( 123  ) );  // => 149
 
 /**
  * Consider a sequence of numbers a0, a1, ..., an, in which an element is equal to the sum of squared digits of the
@@ -67,5 +71,5 @@ function squareDigitsSequence( int ){
 }
 
 console.log( "Square Digits Sequence" );
-console.log( squareDigitsSequence( 16 ) );
-console.log( squareDigitsSequence( 103 ) );
+console.log( "16 =>", squareDigitsSequence( 16 ) );
+console.log( "103 =>", squareDigitsSequence( 103 ) );
