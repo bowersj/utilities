59c933200807654a3a9efb918abe04494be6a8f7
Josh Bowers <josh.bowers.usa@gmail.com>
2019-11-01 00:10:33 -0600

    added notes, needs formating and refactoring

--- node/challenges/foxesAndChickensPoison.js
+++ node/challenges/foxesAndChickensPoison.js
@@ -1,3 +1,49 @@
+/**
+ * Story
+ Old MacDingle had a farm.
+
+ It was a free-range chicken farm with a fox problem, as we learned earlier:
+
+ Ref: The Hunger Games - Foxes and Chickens
+ So old MacDingle decided to lay fox bait around the farm to poison the foxes.
+
+ Foxes F eat chickens C
+
+ When foxes eat fox bait X they die.
+
+ Fox bait is harmless to chickens.
+
+ Chickens in cages [] are safe (unless a fox has got into the cage with them!)
+
+ Kata Task
+ Given the initial configuration of foxes and chickens what will the farm look like the next morning after the hungry foxes have been feasting?
+
+ Examples
+ Ex1 - chickens outside cages when foxes about	Before
+ CCC[CCC]FCC[CCCCC]CFFFF[CCC]FFFF
+ After
+ ...[CCC]F..[CCCCC].FFFF[CCC]FFFF
+ Ex2 - a fox in a chicken cage	Before
+ ...[CCC]...[CCCFC].....[CCC]....
+ After
+ ...[CCC]...[...F.].....[CCC]....
+ Ex3 - foxes are poisoned	Before
+ CCCCC...XCCCCCCCCC....FFF.X..CF
+ After
+ CCCCC...X.................X....
+ Ex4 - a bit of everything	Before
+ ...CC...X...[CCC]CCC[CCCXCCCF]CCCC[CFC]FCC
+ After
+ ...CC...X...[CCC]...[CCCX....]....[.F.]...
+ Notes
+ Anything not a fox, a chicken, fox bait, or a cage is just dirt .
+ All cages are intact (not open-ended), and there are no cages inside other cages
+ The same fox bait can kill any number of foxes
+ A hungry fox will always eat as many chickens as he can get to, before he is tempted by the bait
+
+ https://www.codewars.com/kata/the-hunger-games-foxes-and-chickens-poison/train/javascript/5dbbb27b12e6b0001a32791b
+ */
+
 module.exports = hungryFoxes;
 
 
